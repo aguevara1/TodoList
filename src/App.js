55ab9b0013f25456423320dfcc25431af178aa02
aguevara1 <albguevara12@gmail.com>
2018-09-05 19:53:54 -0500

    not done with todolist

--- src/App.js
+++ src/App.js
@@ -1,51 +1,38 @@
 
-/*
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
-*/
-
-
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class TodoList extends Component {
   // anything you need to store goes in this state
   //because react components are listening to changes in the state
+
   state={
     list:[],
-    inputValue:''
+    inputValue:'',
+  //  check: false,
+  //  strike: 'none'
   };
+
+   //this.state.strike = this.markItemCompleted.bind(this);
+
+   handleItemCompleted(e){
+/*
+     this.setState({check: !this.state.check})
+     if (this.state.strike === 'none'){
+       this.setState({strike: 'line-through'})
+     } else {
+       this.setState({strike: 'none'})
+     }
+     */
+
+   }
 
 
   handleChange=(e)=>{
     this.setState({inputValue:e.target.value});
   }
-
-
- handleClear=(e)=>{
-   this.setState({inputValue:''});
- }
-
 
  handleSubmit=(e) =>{
    e.preventDefault();
@@ -57,24 +44,45 @@ class TodoList extends Component {
    this.setState({list:newList, inputValue:''});
 
  }
+  // <button onClick={this.handleSubmit}>Submit</button>
+  //{this.renderList()}
+  handleDeleteItem(item) {
+    this.setState({
+   list: this.state.list.filter(el => el !== item)
+})
 
- renderList=()=>{
-    return this.state.list.map((item, idx) => {
-        return <li>{idx}-{item}</li>;
-    });
- }
-
+  }
 
   render() {
     return (
-      <div text-align="center">
-        <h2>Teacher Todo List </h2>
-         <form onSubmit={(e)=> this.handleSubmit(e)}>
+      <div className="App">
+        <h2>Teacher To Do List </h2>
+           <div><img src={logo} className="App-logo" alt="logo" /></div>
+
+         <form onSubmit={(e)=> this.handleSubmit(e)}  className="row" >
 
               <input value={this.state.inputValue} type="text" onChange={(e)=>this.handleChange(e)} />
 
-              <button type="submit" value="submit">Enter</button>
+                <button onClick={this.handleSubmit} className="bigButton"
+                disabled={!this.state.inputValue}>{"Add Item #" + (this.state.list.length +1)}</button>
+
         </form>
+        {this.state.list.map((item, index)=>
+            <form className="listItems">
+                <label>
+                 <input name={item} id={index} type="checkbox"
+                    onChange={(e) => this.handleItemCompleted(e)} />
+                   <span> {item} </span>
+                </label>
+                  <button name={item} id={index} type="button" className="btn-danger"
+                  onClick={(e)=>this.handleDeleteItem(item)}>X</button>
+
+            </form>
+              )
+
+            }
+
+
       </div>
     );
   }
